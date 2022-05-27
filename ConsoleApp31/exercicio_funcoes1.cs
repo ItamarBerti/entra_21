383c23f7b6b886b56f6fdde2092a9ea16143c64f
guest01 <guest01@ITELABD17>
2022-05-27 19:37:24 -0300

    array_maior_menor

---
+++ ConsoleApp31/exercicio_funcoes1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        ///  Faça um programa em que o usuário preencha um array de 10 posições e mostre o
+        /// maior valor entre eles(usando funções e/ou procedimentos). 
+
+        static void Main(string[] args)
+        {
+            static void arrayDeNumeros()
+            {
+                float[] notas = new float[10];
+                float maior = 0; 
+
+                for (int i = 0; i < notas.Length; i++)
+                {
+                    Console.WriteLine($"Digite o numero {i}º do seu Array: ");
+                    notas[i] = Convert.ToSingle(Console.ReadLine());
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (notas[i] > maior)
+                    {
+                        maior = notas[i];
+                    }
+                }
+                Console.WriteLine("O maior número é:" + maior);
+
+            }
+            arrayDeNumeros();
+        }
+    }
+}
